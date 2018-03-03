190f4112fa875d701f45a9ce821c46adfcc9c384
pyounuskhan <pyounuskhan@gmail.com>
2018-03-03 12:31:29 +0530

    Initial commit

---
+++ app/src/main/java/ifirst/co/lesson3scrollviewlistview/MainActivity.java
@@ -0,0 +1,29 @@
+package ifirst.co.lesson3scrollviewlistview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+    private String listview_array[] = { "ONE", "TWO", "THREE", "FOUR", "FIVE",
+            "SIX", "SEVEN", "EIGHT", "NINE", "TEN" };
+    ListView myList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myList = (ListView) findViewById(R.id.listView);
+        myList.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listview_array));
+        Helper.getListViewSize(myList);
+
+    }
+}
+
+
+
+
+
+
